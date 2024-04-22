a0ac336e620d721de0ce3188d0d427762aac138a
Jorge López Puebla <jlpenero2005@gmail.com>
2024-04-22 23:49:38 +0200

    Typo

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 
 // NAME CHANGE INFORMATION POPUP
 function showMessage() {
-  var message = document.getElementById("message-alert");
+  var message = document.getElementById("popup-message");
 
   // Sets initiaal opacity
   message.style.opacity = 1;
 
-  setTimeout(function() {
+  setTimeout(function () {
     message.style.opacity = 0;
-  }, 5000); 
+  }, 5000);
 }
 
 // NAME LENGTH CHECK
 // Everytime you type while chosing your name 
 // it checks that it is not longer than 12 letters
-var input = document.getElementById('input');
-input.addEventListener('input', function(event) {
+var input = document.getElementById('name-input');
+input.addEventListener('input', function (event) {
   var characterCount = input.value.length;
 
-  if(characterCount > 12){
+  if (characterCount > 12) {
     window.alert('Name length limit reached');
-    input.value = input.value.substring(0,input.value.length - 1);
+    input.value = input.value.substring(0, input.value.length - 1);
   }
 });
