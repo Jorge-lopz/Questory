6aaf56e8a48f4e64f4c16b6caab54bd4822f6632
Jorge López Puebla <jlpenero2005@gmail.com>
2024-04-24 12:59:34 +0200

    Implemented the first izitoats notifications
    Co-authored-by: JesusSanchez25 <JesusSanchez25@users.noreply.github.com>
    Co-authored-by: Mari0o4 <Mari0o4@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 
 // NAME CHANGE INFORMATION POPUP
 function showMessage() {
-  var message = document.getElementById("popup-message");
-
-  // Sets initiaal opacity
-  message.style.opacity = 1;
-
-  setTimeout(function () {
-    message.style.opacity = 0;
-  }, 5000);
+  iziToast.info({
+    message: 'The chosen name will also be used in-game',
+    position: 'bottomLeft',
+    backgroundColor: '#a7e1ff',
+    displayMode: 2
+  });
 }
 
 // NAME LENGTH CHECK
-// Everytime you type while chosing your name 
-// it checks that it is not longer than 12 letters
+// Everytime you type while chosing your name it checks that it is not longer than 12 letters
 var input = document.getElementById('name-input');
 input.addEventListener('input', function (event) {
   var characterCount = input.value.length;
 
   if (characterCount > 12) {
-    window.alert('Name length limit reached');
+    iziToast.error({
+      message: 'Name length limit reached.',
+      position: 'topRight',
+      backgroundColor: '#fcb4b8',
+      displayMode: 1
+    });
     input.value = input.value.substring(0, input.value.length - 1);
   }
 });
