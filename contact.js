11af6f86c83c6776e8bb3ceecfb0f66d62d1ee20
Jorge López Puebla <jlpenero2005@gmail.com>
2024-05-10 14:35:02 +0200

    3D Fullscreen button

--- contact.js
+++ contact.js
@@ -1,8 +1,14 @@
 import { Application } from './dependencies/splinetool/runtime/build/runtime.js';
 
-
 const canvas = document.getElementById('canvas3d');
+const button = document.getElementById('button');
 
 // Start the application and load the scene
 const spline = new Application(canvas);
 spline.load('https://prod.spline.design/tFmSBFP2EcK41iR4/scene.splinecode');
+
+button.addEventListener('click', () => {
+    canvas.requestFullscreen().catch(err => {
+        alert('Error attempting to enable full-screen mode: ${err.message} (${err.name})');
+    });
+});
